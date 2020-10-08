345d5ec0b9f8b96fcb3d26325634453580654c8e
sarabaghani <sarabaghani72@gmail.com>
2020-10-08 21:00:21 +0600

    Adding logic for 4 in a row game and managing all probable errors.

--- app/src/main/java/com/example/gamebox/FourInaRowFrag.java
+++ app/src/main/java/com/example/gamebox/FourInaRowFrag.java
@@ -9,13 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class FourInaRowFrag extends Fragment {
 
-    private Button b11,b12,b13,b14,b15,b21,b22,b23,b24,b25,b31,b32,b33,b34,b35,
-                    b41,b42,b43,b44,b45,b51,b52,b53,b54,b55;
+    private Button b11, b12, b13, b14, b15, b21, b22, b23, b24, b25, b31, b32, b33, b34, b35,
+            b41, b42, b43, b44, b45, b51, b52, b53, b54, b55;
     private boolean turn = true;
+    int[][] arr = new int[11][11];
+
 
     public FourInaRowFrag() {
         // Required empty public constructor
@@ -28,6 +33,7 @@ public class FourInaRowFrag extends Fragment {
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,9 +41,14 @@ public class FourInaRowFrag extends Fragment {
         View view = inflater.inflate(R.layout.fragment_four_ina_row, container, false);
         findViews(view);
         setListeners();
+        disableButtons();
+        b51.setEnabled(true);
+        b52.setEnabled(true);
+        b53.setEnabled(true);
+        b54.setEnabled(true);
+        b55.setEnabled(true);
         return view;
     }
-
 
 
     private void findViews(View view) {
@@ -67,167 +78,330 @@ public class FourInaRowFrag extends Fragment {
         b54 = view.findViewById(R.id.btn_54);
         b55 = view.findViewById(R.id.btn_55);
     }
+
     private void setListeners() {
         b11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               paint(b11);
+                paint(b11, 1 + 2, 1 + 2);
+                if (isWinner(1 + 2, 1 + 2))
+                    gameFinished(1 + 2, 1 + 2);
             }
         });
         b12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b12);
+                paint(b12, 1 + 2, 2 + 2);
+                if (isWinner(1 + 2, 2 + 2))
+                    gameFinished(1 + 2, 2 + 2);
             }
         });
         b13.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b13);
+                paint(b13, 1 + 2, 3 + 2);
+                if (isWinner(1 + 2, 3 + 2))
+                    gameFinished(1 + 2, 3 + 2);
             }
         });
         b14.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b14);
+                paint(b14, 1 + 2, 4 + 2);
+                if (isWinner(1 + 2, 4 + 2))
+                    gameFinished(1 + 2, 4 + 2);
             }
         });
         b15.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b15);
+                paint(b15, 1 + 2, 5 + 2);
+                if (isWinner(1 + 2, 5 + 2))
+                    gameFinished(1 + 2, 5 + 2);
             }
         });
         b21.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b21);
+                paint(b21, 2 + 2, 1 + 2);
+                b11.setEnabled(true);
+                if (isWinner(2 + 2, 1 + 2)) {
+                    gameFinished(2 + 2, 1 + 2);
+                }
             }
         });
         b22.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b22);
+                paint(b22, 2 + 2, 2 + 2);
+                b12.setEnabled(true);
+                if (isWinner(2 + 2, 2 + 2)) {
+                    gameFinished(2 + 2, 2 + 2);
+                }
             }
         });
         b23.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b23);
+                paint(b23, 2 + 2, 3 + 2);
+                b13.setEnabled(true);
+                if (isWinner(2 + 2, 3 + 2)) {
+                    gameFinished(2 + 2, 3 + 2);
+                }
             }
         });
         b24.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b24);
+                paint(b24, 2 + 2, 4 + 2);
+                b14.setEnabled(true);
+                if (isWinner(2 + 2, 4 + 2)) {
+                    gameFinished(2 + 2, 4 + 2);
+                }
             }
         });
         b25.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b25);
+                paint(b25, 2 + 2, 5 + 2);
+                b15.setEnabled(true);
+                if (isWinner(2 + 2, 5 + 2)) {
+                    gameFinished(2 + 2, 5 + 2);
+                }
             }
         });
         b31.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b31);
+                paint(b31, 3 + 2, 1 + 2);
+                b21.setEnabled(true);
+                if (isWinner(3 + 2, 1 + 2)) {
+                    gameFinished(3 + 2, 1 + 2);
+                }
             }
         });
         b32.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b32);
+                paint(b32, 3 + 2, 2 + 2);
+                b22.setEnabled(true);
+                if (isWinner(3 + 2, 2 + 2)) {
+                    gameFinished(3 + 2, 2 + 2);
+                }
             }
         });
         b33.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b33);
+                paint(b33, 3 + 2, 3 + 2);
+                b23.setEnabled(true);
+                if (isWinner(3 + 2, 3 + 2)) {
+                    gameFinished(3 + 2, 3 + 2);
+                }
             }
         });
         b34.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b34);
+                paint(b34, 3 + 2, 4 + 2);
+                b24.setEnabled(true);
+                if (isWinner(3 + 2, 4 + 2)) {
+                    gameFinished(3 + 2, 4 + 2);
+                }
             }
         });
         b35.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b35);
+                paint(b35, 3 + 2, 5 + 2);
+                b25.setEnabled(true);
+                if (isWinner(3 + 2, 5 + 2)) {
+                    gameFinished(3 + 2, 5 + 2);
+                }
             }
         });
         b41.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b41);
+                paint(b41, 4 + 2, 1 + 2);
+                b31.setEnabled(true);
+                if (isWinner(4 + 2, 1 + 2)) {
+                    gameFinished(4 + 2, 1 + 2);
+                }
             }
         });
         b42.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b42);
+                paint(b42, 4 + 2, 2 + 2);
+                b32.setEnabled(true);
+                if (isWinner(4 + 2, 2 + 2)) {
+                    gameFinished(4 + 2, 2 + 2);
+                }
             }
         });
         b43.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b43);
+                paint(b43, 4 + 2, 3 + 2);
+                b33.setEnabled(true);
+                if (isWinner(4 + 2, 3 + 2)) {
+                    gameFinished(4 + 2, 3 + 2);
+                }
             }
         });
         b44.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b44);
+                paint(b44, 4 + 2, 4 + 2);
+                b34.setEnabled(true);
+                if (isWinner(4 + 2, 4 + 2)) {
+                    gameFinished(4 + 2, 4 + 2);
+                }
             }
         });
         b45.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b45);
+                paint(b45, 4 + 2, 5 + 2);
+                b35.setEnabled(true);
+                if (isWinner(4 + 2, 5 + 2)) {
+                    gameFinished(4 + 2, 5 + 2);
+                }
             }
         });
         b51.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b51);
+                paint(b51, 5 + 2, 1 + 2);
+                b41.setEnabled(true);
+                if (isWinner(5 + 2, 1 + 2)) {
+                    gameFinished(5 + 2, 1 + 2);
+                }
             }
         });
         b52.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b52);
+                paint(b52, 5 + 2, 2 + 2);
+                b42.setEnabled(true);
+                if (isWinner(5 + 2, 2 + 2)) {
+                    gameFinished(5 + 2, 2 + 2);
+                }
             }
         });
         b53.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b53);
+                paint(b53, 5 + 2, 3 + 2);
+                b43.setEnabled(true);
+                if (isWinner(5 + 2, 3 + 2)) {
+                    gameFinished(5 + 2, 3 + 2);
+                }
             }
         });
-
         b54.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b54);
+                paint(b54, 5 + 2, 4 + 2);
+                b44.setEnabled(true);
+                if (isWinner(5 + 2, 4 + 2)) {
+                    gameFinished(5 + 2, 4 + 2);
+                }
             }
         });
-
         b55.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                paint(b55);
+                paint(b55, 5 + 2, 5 + 2);
+                b45.setEnabled(true);
+                if (isWinner(5 + 2, 5 + 2)) {
+                    gameFinished(5 + 2, 5 + 2);
+                }
             }
         });
     }
-public void paint(Button b){
-    if(turn)
-        b.setBackgroundColor(Color.rgb(255,0,0));
-    else
-        b.setBackgroundColor(Color.rgb(0,0,255));
-    turn=!turn;
-    b.setEnabled(false);
-}
+
+    private void paint(Button bt, int a, int b) {
+        if (turn) {
+            bt.setBackgroundColor(Color.rgb(255, 0, 0));
+            arr[a][b] = -1;
+        } else {
+            bt.setBackgroundColor(Color.rgb(0, 0, 255));
+            arr[a][b] = 1;
+        }
+        turn = !turn;
+        bt.setEnabled(false);
+    }
+
+    private boolean isWinner(int a, int b) {
+        int i = arr[a][b];
+        if ((i == arr[a][b + 1] && i == arr[a][b - 1]) && (i == arr[a][b - 2] || i == arr[a][b + 2]))
+            return true;
+        else if ((i == arr[a + 1][b] && i == arr[a - 1][b]) && (i == arr[a - 2][b] || i == arr[a + 2][b]))
+            return true;
+        else if ((i == arr[a + 1][b - 1] && i == arr[a - 1][b + 1]) && (i == arr[a + 2][b - 2] || i == arr[a - 2][b + 2]))
+            return true;
+        else if ((i == arr[a - 1][b - 1] && i == arr[a + 1][b + 1]) && (i == arr[a + 2][b + 2] || i == arr[a - 2][b - 2]))
+            return true;
+        else if (i + arr[a - 1][b] + arr[a - 2][b] + arr[a - 3][b] == 4 || i + arr[a - 1][b] + arr[a - 2][b] + arr[a - 3][b] == -4)
+            return true;
+        else if (i + arr[a + 1][b] + arr[a + 2][b] + arr[a + 3][b] == 4 || i + arr[a + 1][b] + arr[a + 2][b] + arr[a + 3][b] == -4)
+            return true;
+        else if (i + arr[a][b + 1] + arr[a][b + 2] + arr[a][b + 3] == 4 || i + arr[a][b + 1] + arr[a][b + 2] + arr[a][b + 3] == -4)
+            return true;
+        else if (i + arr[a][b - 1] + arr[a][b - 2] + arr[a][b - 3] == 4 || i + arr[a][b - 1] + arr[a][b - 2] + arr[a][b - 3] == -4)
+            return true;
+        else if (i + arr[a + 1][b + 1] + arr[a + 2][b + 2] + arr[a + 3][b + 3] == 4 || i + arr[a + 1][b + 1] + arr[a + 2][b + 2] + arr[a + 3][b + 3] == -4)
+            return true;
+        else if (i + arr[a - 1][b - 1] + arr[a - 2][b - 2] + arr[a - 3][b - 3] == 4 || i + arr[a - 1][b - 1] + arr[a - 2][b - 2] + arr[a - 3][b - 3] == -4)
+            return true;
+        else if (i + arr[a + 1][b - 1] + arr[a + 2][b - 2] + arr[a + 3][b - 3] == 4 || i + arr[a + 1][b - 1] + arr[a + 2][b - 2] + arr[a + 3][b - 3] == -4)
+            return true;
+        else if (i + arr[a - 1][b + 1] + arr[a - 2][b + 2] + arr[a - 3][b + 3] == 4 || i + arr[a - 1][b + 1] + arr[a - 2][b + 2] + arr[a - 3][b + 3] == -4)
+            return true;
+        else
+            return false;
+    }
+
+    private void gameFinished(int a, int b) {
+        String s;
+        if (arr[a][b] == 1)
+            s = "BLUE";
+        else
+            s = "RED";
+        Snackbar.make(getActivity().findViewById(R.id.game_container),
+                s + " won the game !!", Snackbar.LENGTH_LONG)
+                .show();
+        disableButtons();
+    }
+
+    private void disableButtons() {
+        b11.setEnabled(false);
+        b12.setEnabled(false);
+        b13.setEnabled(false);
+        b14.setEnabled(false);
+        b15.setEnabled(false);
+        b21.setEnabled(false);
+        b22.setEnabled(false);
+        b23.setEnabled(false);
+        b24.setEnabled(false);
+        b25.setEnabled(false);
+        b31.setEnabled(false);
+        b32.setEnabled(false);
+        b33.setEnabled(false);
+        b34.setEnabled(false);
+        b35.setEnabled(false);
+        b41.setEnabled(false);
+        b42.setEnabled(false);
+        b43.setEnabled(false);
+        b44.setEnabled(false);
+        b45.setEnabled(false);
+        b51.setEnabled(false);
+        b52.setEnabled(false);
+        b53.setEnabled(false);
+        b54.setEnabled(false);
+        b55.setEnabled(false);
+    }
 
 }
